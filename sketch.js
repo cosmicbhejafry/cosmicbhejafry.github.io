8e7a6479ee0f1818206106e28067763a7ac3a73c
cosmicbhejafry <47458458+cosmicbhejafry@users.noreply.github.com>
2023-07-30 01:25:04 +0100

    add flex, next todo add info and p5

--- sketch.js
+++ sketch.js
@@ -4,33 +4,33 @@ function setup() {
     const canvasContainer = document.getElementById('sketch-canvas');
     const canvasWidth = canvasContainer.clientWidth;
     const canvasHeight = canvasContainer.clientHeight;
-  
+
     const canvas = createCanvas(canvasWidth, canvasHeight);
     canvas.parent('sketch-canvas');
-    background(255);
-  
-    setInterval(updateCanvasPosition, 1000); // Call updateCanvasPosition every 10 seconds
+    background(255);  
+    // setInterval(updateCanvasPosition, 1000); // Call updateCanvasPosition every 10 seconds
   }
-  function updateCanvasPosition() {
-    const canvasContainer = document.getElementById('sketch-canvas');
-    const columnWidth = canvasContainer.clientWidth;
-    const columnHeight = canvasContainer.clientHeight;
+
+  // function updateCanvasPosition() {
+  //   const canvasContainer = document.getElementById('sketch-canvas');
+  //   const columnWidth = canvasContainer.clientWidth;
+  //   const columnHeight = canvasContainer.clientHeight;
   
-    const circleDiameter = 50; // Set the circle diameter
-    const canvasX = random(circleDiameter / 2, columnWidth - circleDiameter / 2);
-    const canvasY = random(circleDiameter / 2, columnHeight - circleDiameter / 2);
+  //   const circleDiameter = 50; // Set the circle diameter
+  //   const canvasX = random(circleDiameter / 2, columnWidth - circleDiameter / 2);
+  //   const canvasY = random(circleDiameter / 2, columnHeight - circleDiameter / 2);
   
-    canvasContainer.style.position = 'absolute';
-    canvasContainer.style.left = canvasX + 'px';
-    canvasContainer.style.top = canvasY + 'px';
-    resizeCanvas(columnWidth, columnHeight);
-    }
+  //   canvasContainer.style.position = 'absolute';
+  //   canvasContainer.style.left = canvasX + 'px';
+  //   canvasContainer.style.top = canvasY + 'px';
+  //   resizeCanvas(columnWidth, columnHeight);
+  // }
   
   function draw() {
     stroke('#000000'); // Black stroke color
     strokeWeight(2); // Set stroke weight to 2 pixels
     ellipse(width / 2, height / 2, min(width, height)/2); // Draw a circle at the center of the canvas
-    resizeCanvas(canvasWidth, canvasHeight);
+    // windowResized();
 }
   
 function windowResized() {
@@ -38,5 +38,6 @@ function windowResized() {
     const canvasWidth = canvasContainer.clientWidth;
     const canvasHeight = canvasContainer.clientHeight;
     resizeCanvas(canvasWidth, canvasHeight);
+    // canvas.log(canvasWidth)
   }
   
